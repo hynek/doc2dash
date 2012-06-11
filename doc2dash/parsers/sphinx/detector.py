450f3edbb609949a8897cba5d79281c4849f3bc7
Hynek Schlawack <hs@ox.cx>
2012-06-11 17:01:52 +0200

    Tune sphinx detector
    Didn't detect pyramid's docs

--- doc2dash/parsers/sphinx/detector.py
+++ doc2dash/parsers/sphinx/detector.py
@@ -3,15 +3,8 @@ import os
 from ..helpers import enoent_means_false
 
 
-HEADER = '''/*
- * searchtools.js
- * ~~~~~~~~~~~~~~
- *
- * Sphinx JavaScript'''
-
-
 @enoent_means_false
 def detect(path):
     """Detect whether *path* is sphinx documentation."""
     with open(os.path.join(path, '_static/searchtools.js')) as f:
-        return f.read(len(HEADER)) == HEADER
+        return  '* Sphinx JavaScript util' in f.read()
