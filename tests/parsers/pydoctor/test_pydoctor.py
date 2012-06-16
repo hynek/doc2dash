9e92edb06380b11a3b890caf793db195de20a63a
Hynek Schlawack <hs@ox.cx>
2012-06-16 19:06:52 +0200

    Get rid of magic strings in tests

--- tests/parsers/pydoctor/test_pydoctor.py
+++ tests/parsers/pydoctor/test_pydoctor.py
@@ -1,54 +1,46 @@
 from mock import patch
 
+from doc2dash.parsers import types
 from doc2dash.parsers.pydoctor.parser import parse, _guess_type
 
 
 def test_guess_type():
-    types = [
-            (
+    ts = [
+            ('startServer',
+             'twisted.conch.test.test_cftp.CFTPClientTestBase.html#'
                 'startServer',
-                'twisted.conch.test.test_cftp.CFTPClientTestBase.html#'
-                 'startServer',
-                'clm'
-            ),
-            (
-                'A',
-                'twisted.test.myrebuilder1.A.html',
-                'cl'
-            ),
-            (
-                'epollreactor',
-                'twisted.internet.epollreactor.html',
-                'cat'
-            )
+             types.METHOD),
+            ('A', 'twisted.test.myrebuilder1.A.html', types.CLASS),
+            ('epollreactor', 'twisted.internet.epollreactor.html',
+             types.PACKAGE)
     ]
 
-    for t in types:
+    for t in ts:
         assert _guess_type(t[0], t[1]) == t[2]
 
 
 EXAMPLE_PARSE_RESULT = [
         ('twisted.conch.insults.insults.ServerProtocol'
-            '.ControlSequenceParser.A', 'clm',
+            '.ControlSequenceParser.A', types.METHOD,
          'twisted.conch.insults.insults.ServerProtocol'
             '.ControlSequenceParser.html#A'),
-        ('twisted.test.myrebuilder1.A', 'cl',
+        ('twisted.test.myrebuilder1.A', types.CLASS,
          'twisted.test.myrebuilder1.A.html'),
-        ('twisted.test.myrebuilder2.A', 'cl',
+        ('twisted.test.myrebuilder2.A', types.CLASS,
          'twisted.test.myrebuilder2.A.html'),
-        ('twisted.test.test_jelly.A', 'cl',
+        ('twisted.test.test_jelly.A', types.CLASS,
          'twisted.test.test_jelly.A.html'),
-        ('twisted.test.test_persisted.A', 'cl',
+        ('twisted.test.test_persisted.A', types.CLASS,
          'twisted.test.test_persisted.A.html'),
-        ('twisted.test.myrebuilder1.A.a', 'clm',
+        ('twisted.test.myrebuilder1.A.a', types.METHOD,
          'twisted.test.myrebuilder1.A.html#a'),
-        ('twisted.test.myrebuilder1.Inherit.a', 'clm',
+        ('twisted.test.myrebuilder1.Inherit.a', types.METHOD,
          'twisted.test.myrebuilder1.Inherit.html#a'),
-        ('twisted.test.myrebuilder2.A.a', 'clm',
+        ('twisted.test.myrebuilder2.A.a', types.METHOD,
          'twisted.test.myrebuilder2.A.html#a'),
-        ('twisted.test.myrebuilder2.Inherit.a', 'clm',
+        ('twisted.test.myrebuilder2.Inherit.a', types.METHOD,
          'twisted.test.myrebuilder2.Inherit.html#a'),
-        ('twisted.web._newclient.HTTP11ClientProtocol.abort', 'clm',
+        ('twisted.web._newclient.HTTP11ClientProtocol.abort', types.METHOD,
          'twisted.web._newclient.HTTP11ClientProtocol.html#abort')
 ]
 
