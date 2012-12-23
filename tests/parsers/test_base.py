aeb800466b152b667ba0f69586e0fbde060a98d7
Hynek Schlawack <hs@ox.cx>
2012-12-23 14:22:58 +0100

    Use py.test's tmpdir feature

--- tests/parsers/test_base.py
+++ tests/parsers/test_base.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 
 from mock import patch
 
@@ -16,27 +15,26 @@ def test_toc_with_empty_db():
     toc.close()
 
 
-def test_add_toc_single_entry(monkeypatch):
+def test_add_toc_single_entry(monkeypatch, tmpdir):
     entries = [
         ('foo', 'clm', 'bar.html#foo'),
         ('qux', 'cl', 'bar.html'),
     ]
-    with tempfile.TemporaryDirectory() as td:
-        monkeypatch.chdir(td)
-        p = TestParser('foo')
-        os.mkdir('foo')
-        with open('foo/bar.html', 'w') as fp:
-            fp.write('docs!')
-        p.find_and_patch_entry = lambda x, y: True
-        toc = p.add_toc()
-        for e in entries:
-            toc.send(e)
-        toc.close()
+    monkeypatch.chdir(tmpdir)
+    p = TestParser('foo')
+    os.mkdir('foo')
+    with open('foo/bar.html', 'w') as fp:
+        fp.write('docs!')
+    p.find_and_patch_entry = lambda x, y: True
+    toc = p.add_toc()
+    for e in entries:
+        toc.send(e)
+    toc.close()
 
-        p.find_and_patch_entry = lambda x, y: False
-        toc = p.add_toc()
-        for e in entries:
-            toc.send(e)
-        with patch('doc2dash.parsers.base.log.debug') as mock:
-            toc.close()
-            assert mock.call_count == 1
+    p.find_and_patch_entry = lambda x, y: False
+    toc = p.add_toc()
+    for e in entries:
+        toc.send(e)
+    with patch('doc2dash.parsers.base.log.debug') as mock:
+        toc.close()
+        assert mock.call_count == 1
