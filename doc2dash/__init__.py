6fdc88deeba09159e3aa44941bbde1de268e3e21
Hynek Schlawack <hs@ox.cx>
2014-08-10 21:47:18 +0200

    Ensure future imports in all files

--- doc2dash/__init__.py
+++ doc2dash/__init__.py
@@ -2,6 +2,8 @@
 Convert docs to Dash.app's docset format.
 """
 
+from __future__ import absolute_import, division, print_function
+
 
 __author__ = 'Hynek Schlawack'
 __version__ = '2.0.0'
--- doc2dash/parsers/types.py
+++ doc2dash/parsers/types.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 ATTRIBUTE = "Attribute"
 CLASS = "Class"
 CONSTANT = "Constant"
