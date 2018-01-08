45c165e23d2833910200b63abf22245a2417957a
Hynek Schlawack <hs@ox.cx>
2018-01-08 17:40:25 +0100

    [requires.io] dependency update (#81)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 NAME = "doc2dash"
 INSTALL_REQUIRES = [
-    "Sphinx==1.6.5",
+    "Sphinx==1.6.6",
     "attrs==17.4.0",
     "beautifulsoup4==4.6.0",
     "click==6.7",
