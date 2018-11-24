89906139e2d269aff197d26646fd3e4f6c33ae20
Hynek Schlawack <hs@ox.cx>
2018-11-24 11:12:43 +0100

    Make flak8 happy

--- setup.py
+++ setup.py
@@ -96,7 +96,9 @@ LONG = (
     + "Release Information\n"
     + "===================\n\n"
     + re.search(
-        "(\d+.\d.\d \(.*?\)\n.*?)\n\n\n----\n\n\n", read("CHANGELOG.rst"), re.S
+        r"(\d+.\d.\d \(.*?\)\n.*?)\n\n\n----\n\n\n",
+        read("CHANGELOG.rst"),
+        re.S,
     ).group(1)
     + "\n\n`Full changelog "
     + "<{url}en/stable/changelog.html>`_.\n\n"
